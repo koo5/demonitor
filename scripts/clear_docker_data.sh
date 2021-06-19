723e9601162f1f703de49491ff11f31d100c97c1
koo5 <kolman.jindrich@gmail.com>
2021-06-19 12:46:19 +0200

    ..

--- scripts/clear_docker_data.sh
+++ scripts/clear_docker_data.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env fish
-rm -rf /var/lib/docker/volumes/damk_*/_data/*
+#
+#rm -rf /var/lib/docker/volumes/damk_*/_data/*/*
+#rm -rf /var/lib/docker/volumes/damk_*/_data/*
+
+docker volume rm damk_ipfs
+docker volume rm damk_orbitdb
+
 
