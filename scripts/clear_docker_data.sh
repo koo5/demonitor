0e017b09f31b221865039e6eca84badd820cf30e
koo5 <kolman.jindrich@gmail.com>
2021-06-19 21:09:16 +0200

    ..

--- scripts/clear_docker_data.sh
+++ scripts/clear_docker_data.sh
@@ -4,6 +4,10 @@
 #rm -rf /var/lib/docker/volumes/damk_*/_data/*
 
 docker volume rm damk_ipfs
+docker volume rm damk_jsipfs
 docker volume rm damk_orbitdb
+docker volume rm damk_ipfs_staging
+docker volume rm damk_ipfs_data
+
 
 
